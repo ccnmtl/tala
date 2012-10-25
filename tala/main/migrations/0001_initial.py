de800513fe84dfa1004ecebeca4c84201759958a
Anders Pearson <anders@columbia.edu>
2012-10-25 13:50:31 -0400

    initial migration for tala.main

---
+++ tala/main/migrations/0001_initial.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Room'
+        db.create_table('main_room', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('group', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.Group'])),
+            ('title', self.gf('django.db.models.fields.CharField')(default=u'unknown room', max_length=256)),
+            ('description', self.gf('django.db.models.fields.TextField')(default=u'', blank=True)),
+        ))
+        db.send_create_signal('main', ['Room'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Room'
+        db.delete_table('main_room')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'main.room': {
+            'Meta': {'object_name': 'Room'},
+            'description': ('django.db.models.fields.TextField', [], {'default': "u''", 'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "u'unknown room'", 'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['main']
--- tala/main/models.py
+++ tala/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# define your models here
+class Room(models.Model):
+    group = models.ForeignKey(Group)
+    title = models.CharField(max_length=256, default=u"unknown room")
+    description = models.TextField(default=u"", blank=True)
+
