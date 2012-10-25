c180c9ecaa9a7a6fdb3e3acca5abc292dd99e9e6
Anders Pearson <anders@columbia.edu>
2012-10-25 14:42:17 -0400

    let user pick a room and enter it

--- tala/main/views.py
+++ tala/main/views.py
@@ -1,6 +1,18 @@
 from annoying.decorators import render_to
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from tala.main.models import User, Room, Message, get_or_create_room
 
 
+@login_required
 @render_to('main/index.html')
 def index(request):
-    return dict()
+    rooms = [get_or_create_room(g) for g in request.user.groups.all()]
+    return dict(rooms=rooms)
+
+
+@login_required
+@render_to('main/room.html')
+def room(request, room_id):
+    room = get_object_or_404(Room, pk=room_id)
+    return dict(room=room)
