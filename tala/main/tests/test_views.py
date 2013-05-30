d2bf5ae5dd2124da7683c9b0332abc6f4ca72f2b
Anders Pearson <anders@columbia.edu>
2013-05-30 11:45:23 -0400

    simple view tests

--- tala/main/tests/test_views.py
+++ tala/main/tests/test_views.py
@@ -1,9 +1,66 @@
 from django.utils import unittest
+from django.test.client import Client
+from django.contrib.auth.models import User, Group
+from tala.main.models import get_or_create_room
 
 
-class BasicViewsTest(unittest.TestCase):
+class LoggedOutTest(unittest.TestCase):
+    """ make sure that we don't have access to anything
+    if we're not logged in """
+
     def setUp(self):
-        pass
+        self.c = Client()
+        self.group = Group.objects.create(name="testgroup")
+        self.room = get_or_create_room(group=self.group)
 
-    def test_dummy(self):
-        assert True
+    def tearDown(self):
+        self.room.delete()
+        self.group.delete()
+
+    def test_root(self):
+        response = self.c.get("/")
+        self.assertEquals(response.status_code, 302)
+
+    def test_room(self):
+        response = self.c.get(self.room.get_absolute_url())
+        self.assertEquals(response.status_code, 302)
+
+    def test_fresh_token(self):
+        response = self.c.get(self.room.get_absolute_url() + "fresh_token/")
+        self.assertEquals(response.status_code, 302)
+
+    def test_post(self):
+        response = self.c.get(self.room.get_absolute_url() + "post/")
+        self.assertEquals(response.status_code, 302)
+
+
+class LoggedInTest(unittest.TestCase):
+    """ make sure that we don't have access to anything
+    if we're not logged in """
+
+    def setUp(self):
+        self.c = Client()
+        self.group = Group.objects.create(name="testgroup")
+        self.room = get_or_create_room(group=self.group)
+        self.user = User.objects.create(username="testuser")
+        self.user.set_password("test")
+        self.user.groups.add(self.group)
+        self.user.save()
+        self.c.login(username="testuser", password="test")
+
+    def tearDown(self):
+        self.room.delete()
+        self.group.delete()
+        self.user.delete()
+
+    def test_root(self):
+        response = self.c.get("/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_room(self):
+        response = self.c.get(self.room.get_absolute_url())
+        self.assertEquals(response.status_code, 200)
+
+    def test_fresh_token(self):
+        response = self.c.get(self.room.get_absolute_url() + "fresh_token/")
+        self.assertEquals(response.status_code, 200)
