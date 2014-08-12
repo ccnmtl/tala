40aa689a6cbd5e8f2b15c8655fb8b9a54a59a8ed
Anders Pearson <anders@columbia.edu>
2014-08-12 11:10:27 +0200

    auth url cleanup

--- tala/urls.py
+++ tala/urls.py
@@ -6,17 +6,9 @@ import os.path
 admin.autodiscover()
 
 site_media_root = os.path.join(os.path.dirname(__file__), "../media")
-
-redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
-auth_urls = (r'^accounts/', include('django.contrib.auth.urls'))
-logout_page = (r'^accounts/logout/$',
-               'django.contrib.auth.views.logout',
-               {'next_page': redirect_after_logout})
-
 urlpatterns = patterns(
     '',
-    auth_urls,
-    logout_page,
+    (r'^accounts/', include('djangowind.urls')),
     (r'^$', 'tala.main.views.index'),
     (r'^room/(?P<room_id>\d+)/$', 'tala.main.views.room'),
     (r'^room/(?P<room_id>\d+)/archive/$', 'tala.main.views.room_archive'),
@@ -26,7 +18,6 @@ urlpatterns = patterns(
     (r'^room/(?P<room_id>\d+)/fresh_token/$', 'tala.main.views.fresh_token'),
     (r'^admin/', include(admin.site.urls)),
     (r'^stats/$', TemplateView.as_view(template_name="stats.html")),
-    (r'^accounts/', include('djangowind.urls')),
     (r'^smoketest/', include('smoketest.urls')),
     (r'^site_media/(?P<path>.*)$',
      'django.views.static.serve', {'document_root': site_media_root}),
