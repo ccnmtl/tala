353688b12e46fa7a3f2d7f1275659e1f3a7ec957
Anders Pearson <anders@columbia.edu>
2015-04-13 12:13:13 +0200

    s3 for static

--- tala/settings_production.py
+++ tala/settings_production.py
@@ -1,9 +1,10 @@
 # flake8: noqa
 from settings_shared import *
 import sys
+import os
 
 TEMPLATE_DIRS = (
-    "/var/www/tala/tala/tala/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 DATABASES = {
@@ -17,11 +18,21 @@ DATABASES = {
         }
 }
 
-MEDIA_ROOT = '/var/www/tala/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/tala/tala/sitemedia'),
-)
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-tala-static-prod"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'diabeaters.s3utils.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + '/media/'
+COMPRESS_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
+
 
 COMPRESS_ROOT = "/var/www/tala/tala/media/"
 DEBUG = False
--- tala/settings_staging.py
+++ tala/settings_staging.py
@@ -1,9 +1,10 @@
 # flake8: noqa
 from settings_shared import *
 import sys
+import os
 
 TEMPLATE_DIRS = (
-    "/var/www/tala/tala/tala/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 DATABASES = {
@@ -17,16 +18,23 @@ DATABASES = {
         }
 }
 
-
-MEDIA_ROOT = '/var/www/tala/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/tala/tala/sitemedia'),
-)
-
-COMPRESS_ROOT = "/var/www/tala/tala/media/"
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-tala-static-stage"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'diabeaters.s3utils.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + '/media/'
+COMPRESS_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
 
 STATSD_PREFIX = 'tala-staging'
 
