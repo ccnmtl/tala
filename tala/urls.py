9388d2532e0c613d537b62e03a67aa2542b1048d
Anders Pearson <anders@columbia.edu>
2016-08-15 12:28:50 +0100

    django 1.10 style urls.py

--- tala/urls.py
+++ tala/urls.py
@@ -1,26 +1,32 @@
-from django.conf.urls import patterns, include
+import django.views.static
+import os.path
+
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
-import os.path
+from tala.main.views import (
+    index, room, room_archive, room_archive_date, post_to_room,
+    fresh_token
+)
+
 admin.autodiscover()
 
 site_media_root = os.path.join(os.path.dirname(__file__), "../media")
-urlpatterns = patterns(
-    '',
-    (r'^accounts/', include('djangowind.urls')),
-    (r'^$', 'tala.main.views.index'),
-    (r'^room/(?P<room_id>\d+)/$', 'tala.main.views.room'),
-    (r'^room/(?P<room_id>\d+)/archive/$', 'tala.main.views.room_archive'),
-    (r'^room/(?P<room_id>\d+)/archive/(?P<date>\d{4}\-\d+\-\d+)/$',
-     'tala.main.views.room_archive_date'),
-    (r'^room/(?P<room_id>\d+)/post/$', 'tala.main.views.post_to_room'),
-    (r'^room/(?P<room_id>\d+)/fresh_token/$', 'tala.main.views.fresh_token'),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^stats/$', TemplateView.as_view(template_name="stats.html")),
-    (r'^smoketest/', include('smoketest.urls')),
-    (r'^site_media/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': site_media_root}),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-)
+urlpatterns = [
+    url(r'^accounts/', include('djangowind.urls')),
+    url(r'^$', index),
+    url(r'^room/(?P<room_id>\d+)/$', room),
+    url(r'^room/(?P<room_id>\d+)/archive/$', room_archive),
+    url(r'^room/(?P<room_id>\d+)/archive/(?P<date>\d{4}\-\d+\-\d+)/$',
+        room_archive_date),
+    url(r'^room/(?P<room_id>\d+)/post/$', post_to_room),
+    url(r'^room/(?P<room_id>\d+)/fresh_token/$', fresh_token),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^stats/$', TemplateView.as_view(template_name="stats.html")),
+    url(r'^smoketest/', include('smoketest.urls')),
+    url(r'^site_media/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': site_media_root}),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.MEDIA_ROOT}),
+]
